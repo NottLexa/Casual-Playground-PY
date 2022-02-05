9c9ed962b4a9b3c57e3b694726cd15be90968b97
Alexey <NotLexa>
2022-02-06 00:36:32 +0300

    Hello world!

---
+++ CASUAL_PLAYGROUND.py
@@ -0,0 +1,71 @@
+import pygame
+#from core import nle as engine
+#from core import compiler as cpc
+import core.nle as engine
+import core.compiler as cpc
+
+#region [ИНИЦИАЛИЗАЦИЯ]
+
+print('''
+
+ _____                       _    ______ _                                             _ 
+/  __ \\                     | |   | ___ \\ |                                           | |
+| /  \\/ __ _ ___ _   _  __ _| |   | |_/ / | __ _ _   _  __ _ _ __ ___  _   _ _ __   __| |
+| |    / _` / __| | | |/ _` | |   |  __/| |/ _` | | | |/ _` | '__/ _ \\| | | | '_ \\ / _` |
+| \\__/\\ (_| \\__ \\ |_| | (_| | |   | |   | | (_| | |_| | (_| | | | (_) | |_| | | | | (_| |
+ \\____/\\__,_|___/\\__,_|\\__,_|_|   \\_|   |_|\\__,_|\\__, |\\__, |_|  \\___/ \\__,_|_| |_|\\__,_|
+                                                  __/ | __/ |                            
+                                                 |___/ |___/                             
+by:                                                                            version:
+  Alexey Kozhanov                                                                      #1
+                                                                               DVLP BUILD
+''')
+
+scale = 30
+WIDTH, HEIGHT = 16*scale, 9*scale
+
+screen = engine.Screen((WIDTH, HEIGHT), (WIDTH*2, HEIGHT*2), 0, True)
+clock = pygame.time.Clock()
+deltatime = 0
+#endregion
+
+#region [SETTINGS]
+#endregion
+
+#region [ROOM]
+room_mainmenu = engine.Room()
+
+engine.rooms.change_current_room(room_mainmenu)
+#endregion
+
+#region [ENTITY]
+#endregion
+
+#region [INSTANCE]
+#endregion
+
+#region [LOOP]
+game_running = True
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # выход
+            game_running = False
+        elif event.type == pygame.VIDEORESIZE:  # изменение размера экрана
+            screen.update_screen((event.w, event.h))
+        elif event.type == pygame.MOUSEMOTION:
+            engine.rooms.current_room.do_mouse_moved(screen.get_mousepos_on_canvas(event.pos))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            engine.rooms.current_room.do_mouse_pressed(screen.get_mousepos_on_canvas(event.pos), event.button)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            engine.rooms.current_room.do_mouse_released(screen.get_mousepos_on_canvas(event.pos), event.button)
+        elif event.type == pygame.KEYDOWN:
+            engine.rooms.current_room.do_kb_pressed(event.key)
+        elif event.type == pygame.KEYUP:
+            engine.rooms.current_room.do_kb_released(event.key)
+
+    screen.get_canvas().fill('black')
+    engine.rooms.current_room.do_step(screen.get_canvas())
+    screen.draw_screen()
+    pygame.display.flip()
+    deltatime = clock.tick()
+#endregion
